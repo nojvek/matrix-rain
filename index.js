6decffd002f6a73213f00f8745c48d9bc10144b0
Noj Vek <noj.vek@mixpanel.com>
2018-12-03 18:36:02 -0800

    Convert to class and separate ansi

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const fs = require(`fs`);
 const ArgumentParser = require(`argparse`).ArgumentParser;
+const ansi = require(`./ansi`);
 const stdout = process.stdout;
 
 const argParser = new ArgumentParser({
@@ -45,45 +46,8 @@ const argParser = new ArgumentParser({
   },
 ].forEach(({flags, opts}) => argParser.addArgument(flags, opts));
 
-const args = argParser.parseArgs();
 
-const ctlEsc = `\x1b[`;
-const ansi = {
-  reset: () => `${ctlEsc}c`,
-  clearScreen: () => `${ctlEsc}2J`,
-  cursorHome: () => `${ctlEsc}H`,
-  cursorPos: (row, col) => `${ctlEsc}${row};${col}H`,
-  cursorVisible: () => `${ctlEsc}?25h`,
-  cursorInvisible: () => `${ctlEsc}?25l`,
-  useAltBuffer: () => `${ctlEsc}?47h`,
-  useNormalBuffer: () => `${ctlEsc}?47l`,
-  underline: () => `${ctlEsc}4m`,
-  off: () => `${ctlEsc}0m`,
-  bold: () => `${ctlEsc}1m`,
-  color: c => `${ctlEsc}${c};1m`,
-
-  colors: {
-    fgRgb: (r, g, b) => `${ctlEsc}38;2;${r};${g};${b}m`,
-    bgRgb: (r, g, b) => `${ctlEsc}48;2;${r};${g};${b}m`,
-    fgBlack: () => ansi.color(`30`),
-    fgRed: () => ansi.color(`31`),
-    fgGreen: () => ansi.color(`32`),
-    fgYellow: () => ansi.color(`33`),
-    fgBlue: () => ansi.color(`34`),
-    fgMagenta: () => ansi.color(`35`),
-    fgCyan: () => ansi.color(`36`),
-    fgWhite: () => ansi.color(`37`),
-    bgBlack: () => ansi.color(`40`),
-    bgRed: () => ansi.color(`41`),
-    bgGreen: () => ansi.color(`42`),
-    bgYellow: () => ansi.color(`43`),
-    bgBlue: () => ansi.color(`44`),
-    bgMagenta: () => ansi.color(`45`),
-    bgCyan: () => ansi.color(`46`),
-    bgWhite: () => ansi.color(`47`),
-  },
-};
-
+// Simple string stream buffer + stdout flush at once
 let outBuffer = [];
 function write(chars) {
   return outBuffer.push(chars);
@@ -98,107 +62,121 @@ function rand(start, end) {
   return start + Math.floor(Math.random() * (end - start));
 }
 
-function generateChars(len, charRange) {
-  // by default charRange == ascii
-  let startCharCode = 0x21;
-  let endCharCode = 0x7E;
-  let emojiChar = String.fromCharCode(0xd83d);
-  if (charRange === `braille`) {
-    startCharCode = 0x2840;
-    endCharCode = 0x28ff;
-  } else if (charRange === `emoji`) {
-    startCharCode = 0xde01;
-    endCharCode = 0xde4a;
-  } else if (charRange === `kanji`) {
-    startCharCode = 0x30a0;
-    endCharCode = 0x30ff;
+
+class MatrixRain {
+  constructor(opts) {
+    this.opts = opts;
+    this.maxSpeed = 20;
+    this.colDroplets = [];
+    this.numCols = 0;
+    this.numRows = 0;
   }
 
-  let chars = new Array(len);
-  let preChar = charRange === `emoji` ? emojiChar : ``;
-  for (let i = 0; i < len; ++i) {
-    chars[i] = preChar + String.fromCharCode(rand(startCharCode, endCharCode));
-  }
+  generateChars(len, charRange) {
+    // by default charRange == ascii
+    let startCharCode = 0x21;
+    let endCharCode = 0x7E;
+    let emojiChar = String.fromCharCode(0xd83d);
 
-  return chars;
-}
-
-const droplets = [];
-const maxSpeed = 20;
-let numCols = 0;
-let numRows = 0;
-
-function makeDroplet(col) {
-  return {
-    col,
-    alive: 0,
-    curRow: rand(0, numRows),
-    height: rand(numRows / 2, numRows),
-    speed: rand(1, maxSpeed),
-    chars: generateChars(numRows, args.charRange),
-  };
-}
-
-function resizeDroplets() {
-  // stdout returns values off by one
-  numCols = stdout.columns;
-  numRows = stdout.rows + 1 ;
-
-  // transpose for direction
-  if (args.direction === `h`) {
-    [numCols, numRows] = [numRows, numCols];
-  }
-
-  // Create droplets per column
-  // add/remove droplets to match column size
-  if (numCols > droplets.length) {
-    for (let col = droplets.length; col < numCols; ++col) {
-      // make two droplets per row
-      droplets.push([makeDroplet(col), makeDroplet(col)]);
+    if (charRange === `braille`) {
+      startCharCode = 0x2840;
+      endCharCode = 0x28ff;
+    } else if (charRange === `emoji`) {
+      startCharCode = 0xde01;
+      endCharCode = 0xde4a;
+    } else if (charRange === `kanji`) {
+      startCharCode = 0x30a0;
+      endCharCode = 0x30ff;
     }
-  } else {
-    droplets.splice(numCols, droplets.length - numCols);
-  }
-}
 
-function writeAt(row, col, str, color) {
-  if (args.direction === `h`) {
-    [row, col] = [col, row];
-  }
-  // Only output if in frame
-  if (row >=0 && row < numRows && col >=0 && col < numCols) {
-    write(`${ansi.cursorPos(row, col)}${color || ``}${str || ``}`);
-  }
-}
+    // emojis are two character widths, so use a prefix
+    let preChar = charRange === `emoji` ? emojiChar : ``;
+    let chars = new Array(len);
+    for (let i = 0; i < len; ++i) {
+      chars[i] = preChar + String.fromCharCode(rand(startCharCode, endCharCode));
+    }
 
-function renderFrame() {
-  const color = args.color;
-  const ansiColor = ansi.colors[`fg${color.charAt(0).toUpperCase()}${color.substr(1)}`]();
-  for (const colDroplets of droplets) {
-    for (const droplet of colDroplets) {
-      const {curRow, col: curCol, height} = droplet;
-      droplet.alive++;
+    return chars;
+  }
 
-      if (droplet.alive % droplet.speed === 0) {
-        writeAt(curRow, curCol, droplet.chars[curRow], ansi.colors.fgWhite());
-        writeAt(curRow - 1, curCol, droplet.chars[curRow - 1], ansiColor);
-        writeAt(curRow - height, curCol, ` `);
-        droplet.curRow++;
+  makeDroplet(col) {
+    return {
+      col,
+      alive: 0,
+      curRow: rand(0, this.numRows),
+      height: rand(this.numRows / 2, this.numRows),
+      speed: rand(1, this.maxSpeed),
+      chars: this.generateChars(this.numRows, this.opts.charRange),
+    };
+  }
+
+  resizeDroplets() {
+    // stdout returns values off by one
+    this.numCols = stdout.columns;
+    this.numRows = stdout.rows + 1 ;
+
+    // transpose for direction
+    if (this.opts.direction === `h`) {
+      [this.numCols, this.numRows] = [this.numRows, this.numCols];
+    }
+
+    // Create droplets per column
+    // add/remove droplets to match column size
+    if (this.numCols > this.colDroplets.length) {
+      for (let col = this.colDroplets.length; col < this.numCols; ++col) {
+        // make two droplets per row that start in random positions
+        this.colDroplets.push([this.makeDroplet(col), this.makeDroplet(col)]);
       }
-
-      if (curRow - height > numRows) {
-        // reset droplet
-        Object.assign(droplet, makeDroplet(droplet.col), {curRow: 0});
-      }
+    } else {
+      this.colDroplets.splice(this.numCols, this.colDroplets.length - this.numCols);
     }
   }
-  flush();
+
+  writeAt(row, col, str, color) {
+    if (this.opts.direction === `h`) {
+      [row, col] = [col, row];
+    }
+    // Only output if in viewport
+    if (row >=0 && row < this.numRows && col >=0 && col < this.numCols) {
+      write(`${ansi.cursorPos(row, col)}${color || ``}${str || ``}`);
+    }
+  }
+
+  renderFrame() {
+    const color = this.opts.color;
+    const ansiColor = ansi.colors[`fg${color.charAt(0).toUpperCase()}${color.substr(1)}`]();
+
+    for (const droplets of this.colDroplets) {
+      for (const droplet of droplets) {
+        const {curRow, col: curCol, height} = droplet;
+        droplet.alive++;
+
+        if (droplet.alive % droplet.speed === 0) {
+          this.writeAt(curRow, curCol, droplet.chars[curRow], ansi.colors.fgWhite());
+          this.writeAt(curRow - 1, curCol, droplet.chars[curRow - 1], ansiColor);
+          this.writeAt(curRow - height, curCol, ` `);
+          droplet.curRow++;
+        }
+
+        if (curRow - height > this.numRows) {
+          // reset droplet
+          Object.assign(droplet, this.makeDroplet(droplet.col), {curRow: 0});
+        }
+      }
+    }
+
+    flush();
+  }
 }
+
 
 //// main ////
 
+const args = argParser.parseArgs();
+const matrixRain = new MatrixRain(args);
+
 stdout.on(`resize`, function() {
-  return resizeDroplets();
+  matrixRain.resizeDroplets();
 });
 
 process.on(`SIGINT`, function() {
@@ -206,14 +184,15 @@ process.on(`SIGINT`, function() {
   write(ansi.cursorHome());
   write(ansi.useNormalBuffer());
   flush();
-  return process.exit();
+  process.exit();
 });
 
+// clear terminal and use alt buffer
 write(ansi.useAltBuffer());
 write(ansi.cursorInvisible());
-write(ansi.clearScreen());
 write(ansi.colors.bgBlack());
+write(ansi.clearScreen());
 flush();
-resizeDroplets();
+matrixRain.resizeDroplets();
 
-setInterval(renderFrame, 16); // 60FPS
+setInterval(() => matrixRain.renderFrame(), 16); // 60FPS
